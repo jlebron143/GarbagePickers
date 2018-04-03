ce174969af8095d251cf78226ca756090db89de2
jlebron143 <jlebron143@gmail.com>
2018-04-03 13:15:31 -0500

    added RoleViewModel

---
+++ GarbagePickers/Migrations/201804031813554_added RoleViewModel.cs
@@ -0,0 +1,18 @@
+namespace GarbagePickers.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedRoleViewModel : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetRoles", "Discriminator", c => c.String(nullable: false, maxLength: 128));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetRoles", "Discriminator");
+        }
+    }
+}
--- GarbagePickers/Models/RoleViewModel.cs
+++ GarbagePickers/Models/RoleViewModel.cs
@@ -7,6 +7,15 @@ namespace GarbagePickers.Models
 {
     public class RoleViewModel
     {
+        public RoleViewModel() { }
 
+        public RoleViewModel(ApplicationRole role)
+        {
+            Id = role.Id;
+            Name = role.Name;
+        }
+
+        public string Id { get; set; }
+        public string Name  { get; set; }
     }
 }
