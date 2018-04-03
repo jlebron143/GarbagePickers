69a96d7b208f0197826d2c9f0155522aa2a67f81
jlebron143 <jlebron143@gmail.com>
2018-04-03 12:37:07 -0500

    Add project files.

---
+++ GarbagePickers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GarbagePickers.Startup))]
+namespace GarbagePickers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
